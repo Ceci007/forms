683a43ef3950204e2791403b5ce7f299cb5297d2
solbencas <cecibenitezca@gmail.com>
2020-10-06 14:12:13 -0300

    Create first user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+  
   def new 
+    @user = User.new
   end
 
-  def create 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "Welcome to Blogger-Ceci-App #{@user.username}, you have successfully signed up."
+    else
+      render 'new'
+    end
+  end
+
+  private 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
